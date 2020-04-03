b0d9a7627e526d24c76c5ecdf54b61fb0f65efbd
marvin255 <postbox433@gmail.com>
2020-04-03 16:59:41 +0200

    add empty test for entity manager

---
+++ tests/EntityManager/BaseEntityManagerTest.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Liquetsoft\Fias\Elastic\Tests\EntityManager;
+
+use Liquetsoft\Fias\Elastic\Tests\BaseCase;
+use Liquetsoft\Fias\Elastic\EntityManager\BaseEntityManager;
+
+/**
+ * Набор тестов для объекта, который управляет записью документов в хранилище.
+ */
+class BaseEntityManagerTest extends BaseCase
+{
+
+}
