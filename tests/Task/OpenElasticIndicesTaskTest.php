c1b6cb95a8c3a7959bcac1641eb838413c32024f
marvin255 <postbox433@gmail.com>
2020-04-07 16:01:41 +0200

    add open adn close indices tasks (#19)

---
+++ tests/Task/OpenElasticIndicesTaskTest.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Liquetsoft\Fias\Elastic\Tests\Task;
+
+use Liquetsoft\Fias\Component\Pipeline\State\State;
+use Liquetsoft\Fias\Elastic\IndexBuilder\IndexBuilder;
+use Liquetsoft\Fias\Elastic\IndexMapperInterface;
+use Liquetsoft\Fias\Elastic\IndexMapperRegistry\IndexMapperRegistry;
+use Liquetsoft\Fias\Elastic\Task\OpenElasticIndicesTask;
+use Liquetsoft\Fias\Elastic\Tests\BaseCase;
+use Throwable;
+
+/**
+ * Тест для операции, которая помечает индексы открытыми для записи.
+ */
+class OpenElasticIndicesTaskTest extends BaseCase
+{
+    /**
+     * Проверяет, что операция откроет все индексы.
+     *
+     * @throws Throwable
+     */
+    public function testRun()
+    {
+        $mapper = $this->getMockBuilder(IndexMapperInterface::class)->getMock();
+        $mapper1 = $this->getMockBuilder(IndexMapperInterface::class)->getMock();
+
+        $mapperRegistry = $this->getMockBuilder(IndexMapperRegistry::class)->getMock();
+        $mapperRegistry->method('getAllMappers')->will($this->returnValue([$mapper, $mapper1]));
+
+        $indexBuilder = $this->getMockBuilder(IndexBuilder::class)->getMock();
+        $indexBuilder->expects($this->at(0))->method('open')->with($this->identicalTo($mapper));
+        $indexBuilder->expects($this->at(1))->method('open')->with($this->identicalTo($mapper1));
+
+        $state = $this->getMockBuilder(State::class)->getMock();
+
+        $task = new OpenElasticIndicesTask($mapperRegistry, $indexBuilder);
+        $task->run($state);
+    }
+}
